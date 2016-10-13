d11ad76d15195d2db71441f5d0ce8b1d0b3a6ec3
Prashant Koshta <prashantkoshta@gmail.com>
2016-10-12 22:44:27 -0500

    Configure grunt to create release build.

---
+++ gruntfile.js
@@ -0,0 +1,22 @@
+module.exports = function(grunt) {
+  require('load-grunt-tasks')(grunt);
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    clean: {
+        release: ['release/*']
+    },
+    compress: {
+        main: {
+            options: {
+            archive: 'release/chrome-web-page-downloader.zip'
+            },
+            files: [
+            {src: ['images/**','js/**','downloadpopup.html','icon.png','manifest.json'], dest: '/'}
+            ]
+        }
+    }
+  });
+
+  grunt.registerTask('default', ['clean','compress']);
+
+};
